061e9660379fd480ce803f520aec82f9c38c2047
Stuart Wong <cgs.wong@gmail.com>
2014-09-19 22:14:07 +0000

    Switched Garbage Collector

--- jms/setUserOverrides.sh
+++ jms/setUserOverrides.sh
@@ -12,16 +12,24 @@
 #       the DOMAIN_HOME/bin directory.                            #
 #                                                                 #
 # LOG:                                                            #
-# yyyy/mm/dd [name]: [version]-[notes]                            #
-# 2014/09/04 cgwong: v0.1.0-Initial creation                      #
+# yyyy/mm/dd [name] [version]:[notes]                             #
+# 2014/09/04 cgwong v0.1.0: Initial creation                      #
+# 2014/09/18 cgwong v0.1.1: Added "-XX:-UseGCOverheadLimit" flag  #
+#            to fix JVM OOM error.                                #
+# 2014/09/18 cgwong v0.2.0: Switched to Garbage-First or G1       #
+#            collector. Added additional GC flags for diagnostics #
+#            and logging.                                         #
 ###################################################################
 
-# -- Additional java command line options for servers
+# Custom variables
+DIAG_BASE=/www/web/weblogs/${DOMAIN_HOME}
+SERVER_NAME=<server_name>
+
 # Enabled monitoring, unlocking commercial features
 MONITOR_ARGS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
 
 # Admin and managed server memory settings
-AS_MEM_SIZE="8G"
+AS_MEM_SIZE="4G"
 MS_MEM_SIZE="8G"
 AS_MEM_ARGS="-Xms${AS_MEM_SIZE} -Xmx${AS_MEM_SIZE}"
 MS_MEM_ARGS="-Xms${MS_MEM_SIZE} -Xmx${MS_MEM_SIZE}"
@@ -31,7 +39,8 @@ MEM_MAX_PERM_SIZE="-XX:MaxPermSize=256m"
 MEM_DEV_ARGS="-XX:CompileThreshold=8000 ${MEM_PERM_SIZE} ${MEM_MAX_PERM_SIZE}"
 
 # Garbage Collection related settings
-GC_ARGS="-verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+##GC_ARGS="-verbose:gc -XX:-UseGCOverheadLimit-XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+GC_ARGS="-verbose:gc -XX:+UseG1GC -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${DIAG_BASE}/${SERVER_NAME} -Xloggc:${DIAG_BASE}/${SERVER_NAME}/gc-$$.log -XX:+UseGCLogFileRotation -XX:NumberOfGClogFiles=5 -XX:GCLogFileSize=10240K"
 
 # Custom arguments
 CUST_ARGS="-XX:+UseTLAB"
@@ -48,7 +57,7 @@ export USER_MEM_ARGS
 # Set DERBY_FLAG
 DERBY_FLAG="false" ; export DERBY_FLAG
 
-# Java VM to usr
+# Java VM to server
 JAVA_VM="-server" ; export JAVA_VM
 
 # -- END -- #
